138e90401ce63b18ca1366811133e9148433d10d
Mayn <MaryW2893492@outlook.com>
2026-07-03 09:27:29 +0800

    xxx

---
+++ app/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 40, color: "white", background: "#0b0f19" }}>
+      <h1>工业AI色卡详情页</h1>
+      <p>这里是动态详情页面</p>
+    </div>
+  );
+}
